a6082b1096d4a5f4c53b7e0cdd4a9f4ae9f27d99
Sumant Manne <sumant.manne@gmail.com>
2017-04-06 13:44:51 -0500

    Added additional tests, /data, chown, .vscode

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,21 @@ FROM python:3.6-alpine
 LABEL maintainer Sumant Manne <sumant.manne@gmail.com>
 LABEL description Docker image for the Limnoria IRC bot
 
-RUN adduser -S limnoria
-
 RUN python3 -m pip install --no-cache-dir --upgrade limnoria
 
 COPY ["start.sh", "/usr/local/bin/"]
 RUN chmod +x /usr/local/bin/start.sh
 
+RUN adduser -S limnoria && \
+    mkdir -p /data && \
+    chown limnoria /data
+
 USER limnoria
 
-VOLUME ["/var/limnoria"]
-WORKDIR /var/limnoria
+VOLUME ["/data"]
+WORKDIR /data
 
 CMD ["/usr/local/bin/start.sh"]
+
+# ONBUILD ARG CONFIG_FOLDER=config
+# ONBUILD ADD [CONFIG_FOLDER] .
--- start.sh
+++ start.sh
@@ -8,14 +8,17 @@ NUM_CONF_FILES=$(find . -maxdepth 1 -name '*.conf' | wc -l)
 
 if [ "$NUM_CONF_FILES" -eq 1 ]; then
     CONF_FILE=$(find . -maxdepth 1 -name '*.conf' | head -n 1)
-    supybot "$CONF_FILE"
+
+    exec supybot "$CONF_FILE" "$@"
 elif [ "$NUM_CONF_FILES" -gt 1 ]; then
     >&2 echo "Multiple configuration files (*.conf) for Limnoria found. Remove the extra configurations and try again."
+
+    exit 1
 else
     # Check if we have a stdin allocated to use for the wizard
-    if [ ! -t 0 ] ; then
+    if [ ! -t 0 ]; then
         >&2 echo "Warning: No TTY detected but we need to run the wizard. Try running this container with the -t flag."
-        >&2 echo "Example: \"docker run -v ~/limnoria:/var/limnoria --rm -it limnoria\""
+        >&2 echo "Example: \"docker run -v ~/limnoria:/data --rm -it limnoria\""
         >&2 echo ""
     fi
 
